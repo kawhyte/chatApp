c93967720daf980c52dff320a24bcc757020d387
Kenny Whyte <kawhyte@gmail.com>
2019-10-28 10:31:08 -0700

    Added Query Dom code

--- public/chat.js
+++ public/chat.js
@@ -1,3 +1,9 @@
 //Make connection
 
 const client_socket = io.connect("http://localhost:4000/");
+
+// Query DOM
+var message = document.getElementById('message'),
+      handle = document.getElementById('handle'),
+      btn = document.getElementById('send'),
+      output = document.getElementById('output');
