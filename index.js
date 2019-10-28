ed19dd91b0642168463009200ae93e686653c018
Kenny <kawhyte@gmail.com>
2019-10-28 11:29:25 -0700

    added Message Preview

--- index.js
+++ index.js
@@ -1,26 +1,29 @@
-const express = require('express');
-const socket = require('socket.io');
-
+const express = require("express");
+const socket = require("socket.io");
 
 // App setup
 const app = express();
 
-const server = app.listen(4000,()=>{
-    console.log("listening on port 4000")
-})
+const server = app.listen(4000, () => {
+  console.log("listening on port 4000");
+});
 
 //Static Files
-app.use(express.static("public"))
+app.use(express.static("public"));
 
 //socket setup
 const io = socket(server);
 
-io.on('connection', (socket)=>{
-    console.log("socket.io connection",socket.id)
+io.on("connection", socket => {
+  console.log("socket.io connection", socket.id);
 
-    socket.on('chat', (data)=>{
-        console.log(data)
-io.sockets.emit('chat', data)
+  socket.on("chat", data => {
+    console.log(data);
+    io.sockets.emit("chat", data);
+  });
 
-    })
-})
+  // Handle typing event
+  socket.on("typing", function(data) {
+    socket.broadcast.emit("typing", data);
+  });
+});
--- public/chat.js
+++ public/chat.js
@@ -6,7 +6,8 @@ const socket = io.connect("http://localhost:4000/");
 const message = document.getElementById("message"),
   handle = document.getElementById("handle"),
   btn = document.getElementById("send"),
-  output = document.getElementById("output");
+  output = document.getElementById("output"),
+  feedback = document.getElementById('feedback');
 
 //Emit Event
 btn.addEventListener("click", (e) => {
@@ -19,8 +20,16 @@ btn.addEventListener("click", (e) => {
   message.value ="";
 });
 
+message.addEventListener('keypress', ()=> {
+  socket.emit('typing', handle.value);
+})
+
 //Listen for events
 socket.on('chat', (data)=>{
-  
+  feedback.innerHTML = '';
       output.innerHTML += '<p><strong>' + data.handle + ': </strong>' + data.message + '</p>';
 })
+
+socket.on('typing', function(data){
+  feedback.innerHTML = '<p><em>' + data + ' is typing a message...</em></p>';
+});
